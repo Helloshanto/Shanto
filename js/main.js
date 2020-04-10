4a79fdf02e2b8d5b54cb6af62ef43b44bec3f153
Helloshanto <shantoprogrammer>
2020-04-10 11:30:15 +0600

    add portfolio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    watchSlidesProgress: true,
+    centeredSlidesBounds: true,
+    centeredSlides: true,
+    loop: true,
+  });
